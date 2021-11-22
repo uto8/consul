a6a0adcd7e7eeaa53b16f2028ee5efb10f32ce61
uto8 <70425214+uto8@users.noreply.github.com>
2021-11-22 14:33:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ $(function () {
     $(this).next().slideToggle();
   });
 });
+
+
+$(window).scroll(function () {
+  var height = $(window).height();
+  if ($(window).scrollTop() > height) {
+    $('#header').addClass('view');
+  } else {
+    $('#header').removeClass('inview');
+  }
+});
